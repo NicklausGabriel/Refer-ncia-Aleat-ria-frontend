f962e6c746bc60e70a19f9bd3b9c4ba20bf689ce
gabriel.avelar <gabriel.avelar@parcelaexpress.com.br>
2026-01-08 12:20:41 -0300

    Melhorando layout para computador

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { Button, Card, Flex, Image, Skeleton, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Text
+} from "@chakra-ui/react";
 import React from "react";
 import "./App.css";
 import { getImageUrl } from "./clients/RandomImage";
-import gifInitial from "./assets/initialImage.gif";
 
 function App() {
   const [imageLoaded, setImageLoaded] = React.useState(true);
@@ -26,56 +31,117 @@ function App() {
 
   const awitNextReference = (image.url && !imageLoaded) || false;
   return (
-    <Card.Root
-      w={{ base: "100vw", md: "lg" }}
-      h={{ base: "100%", md: "auto" }}
-      maxW={{ base: "100vw", md: "lg" }}
-      borderRadius={{ base: "0", md: "xl" }}
-      boxShadow={{ base: "none", md: "lg" }}
-      overflow="hidden"
-    >
-      {!image.url && (
-        <Image src={gifInitial} alt="initial-gif" objectFit="cover" />
-      )}
-      <Skeleton loading={!imageLoaded} borderRadius="md">
-        <Image
-          src={image.url}
-          alt={image.author}
-          onLoad={() => setImageLoaded(true)}
-          objectFit="cover"
-        />
-      </Skeleton>
-      <Card.Body gap="2">
-        {imageLoaded && image.url ? (
-          <Flex alignItems="center" gap="1rem">
-            <Card.Title>Autor:</Card.Title>
-            <Card.Description>{image.author}</Card.Description>
-          </Flex>
-        ) : (
-          <Text
-            background="#6a0810"
-            color="white"
-            textAlign="center"
-            fontWeight="bold"
-          >
-            Um clique, uma nova referência visual.
-          </Text>
-        )}
-      </Card.Body>
-      <Card.Footer gap="2">
-        <Button
-          width="100%"
-          variant="solid"
-          loading={loading}
-          disabled={awitNextReference}
-          onClick={() => {
-            getNewReference();
-          }}
+    <Flex width="100%" height="100%" justifyContent="space-between">
+      <Box background="#B8451F" width="30%" color="#ede5da" padding="3rem">
+        <Flex
+          border="1px solid #ede5da"
+          height="100%"
+          padding="1rem"
+          flexDirection="column"
+          justifyContent="space-between"
         >
-          Clique aqui para gerar referência
-        </Button>
-      </Card.Footer>
-    </Card.Root>
+          <Flex justifyContent="flex-end">
+            <Text
+              fontFamily="'Playfair Display', serif"
+              fontSize="20px"
+              fontWeight="700"
+              maxWidth="fit-content"
+              textAlign="right"
+            >
+              Clique
+              <br />
+              Desenhe
+            </Text>
+          </Flex>
+          <Image src="https://res.cloudinary.com/dmqy0gmqw/image/upload/v1767885277/e12e9b0c-c912-48be-bc01-0afd2577dcd3.png" />
+          <Flex justifyContent="flex-start" gap=".3rem">
+            <Text
+              fontFamily="'Playfair Display', serif"
+              fontSize="sm"
+              fontWeight="700"
+              maxWidth="fit-content"
+              textAlign="left"
+            >
+              Criado por:
+            </Text>
+            <Text
+              fontFamily="'Playfair Display', serif"
+              fontSize="sm"
+              maxWidth="fit-content"
+              textAlign="left"
+            >
+              Gabriel Ferreira
+              <br />
+              Belo horizonte
+              <br />
+              2025
+            </Text>
+          </Flex>
+          <Button
+            borderRadius={0}
+            background="#1F4E2E"
+            _hover={{ background:"#4b795aff"}}
+            marginTop="1rem"
+            fontWeight="bold"
+            variant="solid"
+            loading={loading}
+            disabled={awitNextReference}
+            onClick={() => {
+              getNewReference();
+            }}
+          >
+            Clique aqui para gerar referência
+          </Button>
+        </Flex>
+      </Box>
+      <Box background="#ede5da" width="70%" position="relative">
+        {image.url ? (
+          <Image
+            src={image.url}
+            alt={image.author}
+            onLoad={() => setImageLoaded(true)}
+            width="100%"
+            height="100%"
+            objectFit="cover"
+          />
+        ) : (
+          <Flex
+            width="100%"
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Text
+              fontFamily="'Playfair Display', serif"
+              fontSize="96px"
+              fontWeight="700"
+              letterSpacing="-2px"
+              color="#B8451F"
+            >
+              Ideia.
+            </Text>
+          </Flex>
+        )}
+
+        <Box
+          position="absolute"
+          bottom="1"
+          right="1"
+          zIndex={1}
+          background="rgba(0, 0, 0, 0.5)"
+          padding="0.75rem 1rem"
+        >
+          <Flex alignItems="center" gap="0.5rem" color="white">
+            <Text fontWeight="bold">Autor:</Text>
+            {image.url ? (
+              <Text>{image.author}</Text>
+            ) : (
+              <Text>Gabriel Ferreira</Text>
+            )}
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
 
